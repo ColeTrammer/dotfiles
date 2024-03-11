2b67a0c5a204cf9925499d3f5cd79b28ce71b4e9
ColeTrammer <cole.trammer@gmail.com>
2024-03-11 00:20:07 -0700

    Inhibit idle when audio is playing
    Note that this is not necessary for steam, but is for watching videos
    with Firefox.

--- home/desktop/hypridle.nix
+++ home/desktop/hypridle.nix
@@ -4,7 +4,14 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  onlyIfNoAudio = pkgs.writeShellScript "only-if-no-audio" ''
+    ${pkgs.pipewire}/bin/pw-cli i all 2>&1 | ${pkgs.ripgrep}/bin/rg running -q
+    if [ $? == 1 ]; then
+      "$@"
+    fi
+  '';
+in {
   imports = [
     inputs.hypridle.homeManagerModules.default
   ];
@@ -13,21 +20,21 @@
     enable = true;
     beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
     afterSleepCmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-    lockCmd = "pidof hyprlock || ${lib.getExe config.programs.hyprlock.package}";
+    lockCmd = "${pkgs.procps}/bin/pidof hyprlock || ${lib.getExe config.programs.hyprlock.package}";
 
     listeners = [
       {
         timeout = 300;
-        onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
+        onTimeout = "${onlyIfNoAudio} ${pkgs.systemd}/bin/loginctl lock-session";
       }
       {
         timeout = 380;
-        onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+        onTimeout = "${onlyIfNoAudio} ${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
       {
         timeout = 1800;
-        onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
+        onTimeout = "${onlyIfNoAudio} ${pkgs.systemd}/bin/systemctl suspend";
       }
     ];
   };
