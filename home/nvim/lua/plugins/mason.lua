d151f5e00b74a26115bca72a5cc23774dfbb675e
ColeTrammer <cole.trammer@gmail.com>
2024-03-25 23:39:02 -0700

    Enable mason but don't install anything

--- home/nvim/lua/plugins/mason.lua
+++ home/nvim/lua/plugins/mason.lua
@@ -1,5 +1,21 @@
 return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason.nvim", opts = {
+    ensure_installed = {},
+  } },
   { "williamboman/mason-nvim-dap.nvim", enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      for server, server_opts in pairs(opts.servers) do
+        server_opts.mason = false
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      -- Disable default behavior of using mason.
+    end,
+  },
 }
