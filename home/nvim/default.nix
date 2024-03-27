a4a1860ce558ef0c2808aadc207f94dbd3106de3
ColeTrammer <cole.trammer@gmail.com>
2024-03-26 22:50:56 -0700

    Work around out of store symlink issue

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -4,46 +4,40 @@
   pkgs,
   ...
 }: {
-  options = {
-    useMkOutOfStoreSymlink = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = ''Use out of store symlink to link the config.'';
-    };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
+    vimdiffAlias = true;
+
+    # Node JS is required for GitHub copilot.
+    # wl-clipboard is required for copy/paste to work on wayland desktops.
+    # nil and alejandra are installed so that we can setup nix dev environments while still having formatting + LSP.
+    extraPackages = with pkgs; [
+      nodejs_21
+      wl-clipboard
+      nil
+      alejandra
+    ];
   };
 
-  config = {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-      viAlias = true;
-      vimdiffAlias = true;
+  # We can't use lib.mkOutOfStoreSymlink because of a nix unstable issue.
+  # see https://github.com/nix-community/home-manager/issues/4692
+  home.activation = {
+    updateLinks = ''
+      export ROOT="${config.home.homeDirectory}/Workspace/nix/dotfiles"
+      mkdir -p .config
+      ln -sf "$ROOT/home/nvim" .config/nvim
+    '';
+  };
 
-      # Node JS is required for GitHub copilot.
-      # wl-clipboard is required for copy/paste to work on wayland desktops.
-      # nil and alejandra are installed so that we can setup nix dev environments while still having formatting + LSP.
-      extraPackages = with pkgs; [
-        nodejs_21
-        wl-clipboard
-        nil
-        alejandra
-      ];
-    };
-
-    # Out of store symlink so config is modifiable.
-    xdg.configFile.nvim.source =
-      if config.useMkOutOfStoreSymlink
-      then config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Workspace/nix/dotfiles/home/nvim"
-      else ./.;
-
-    # Persist nvim data. .local/share/nvim is only needed for the Lazy and Mason package managers.
-    home.persistence."/persist/home" = {
-      allowOther = true;
-      directories = [
-        ".local/state/nvim"
-        ".local/share/nvim"
-      ];
-    };
+  # Persist nvim data. .local/share/nvim is only needed for the Lazy and Mason package managers.
+  home.persistence."/persist/home" = {
+    allowOther = true;
+    directories = [
+      ".local/state/nvim"
+      ".local/share/nvim"
+    ];
   };
 }
