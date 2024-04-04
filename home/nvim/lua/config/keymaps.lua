a2b1c65341387ff0091f646e7e6c11bfa5ccdf3f
ColeTrammer <cole.trammer@gmail.com>
2024-04-03 19:50:06 -0700

    Remove copilot

--- home/nvim/lua/config/keymaps.lua
+++ home/nvim/lua/config/keymaps.lua
@@ -12,21 +12,5 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
 -- Replace word under cursor.
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- Toggle Copilot with <leader>cp
-vim.g.GitHubCopilotEnabled = true
-local function copilot_toggle()
-  if vim.g.GitHubCopilotEnabled then
-    require("copilot.command").disable()
-  else
-    require("copilot.command").enable()
-  end
-
-  vim.g.GitHubCopilotEnabled = not vim.g.GitHubCopilotEnabled
-end
-
--- Start disabled
-copilot_toggle()
-
-vim.keymap.set("n", "<leader>cp", copilot_toggle, { noremap = true, desc = "Toggle GitHub Copilot" })
-
+-- Treat shift+enter and ctrl+enter as regular enter.
 vim.keymap.set({ "i", "s" }, "<C-^>", "<C-m>")
