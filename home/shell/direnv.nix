15f34bad6c345667eb45631cfda21579a6a7bc90
ColeTrammer <cole.trammer@gmail.com>
2024-05-12 13:43:25 -0700

    Add direnv hook for auto loading zsh completions

--- home/shell/direnv.nix
+++ home/shell/direnv.nix
@@ -26,5 +26,29 @@
         }
       ];
     };
+
+    # Make sure to do this last.
+    programs.zsh.initExtra = lib.mkOrder 2000 ''
+      ###
+      ### Direnv completion autoloading (see https://github.com/direnv/direnv/issues/443)
+      ###
+      export FPATH
+      export XDG_DATA_DIRS
+
+      _direnv_hook() {
+        trap -- ''' SIGINT
+        eval "$(${config.programs.direnv.package}/bin/direnv export zsh)"
+        compinit
+        trap - SIGINT
+      }
+      typeset -ag precmd_functions
+      if (( ! ''${precmd_functions[(I)_direnv_hook]} )); then
+        precmd_functions=(_direnv_hook $precmd_functions)
+      fi
+      typeset -ag chpwd_functions
+      if (( ! ''${chpwd_functions[(I)_direnv_hook]} )); then
+        chpwd_functions=(_direnv_hook $chpwd_functions)
+      fi
+    '';
   };
 }
