a292cd00cee053f436868bfedc9d8b544e370509
ColeTrammer <cole.trammer@gmail.com>
2024-06-06 18:26:17 -0700

    Add keybindings for neogit

--- home/nvim/ui/neogit.nix
+++ home/nvim/ui/neogit.nix
@@ -3,5 +3,47 @@
     plugins.neogit = {
       enable = true;
     };
+    keymaps = [
+      {
+        key = "<leader>gg";
+        action = {
+          __raw = ''
+            function()
+              local neogit = require('neogit')
+              neogit.open()
+            end
+          '';
+        };
+        mode = "n";
+        options = {desc = "Git";};
+      }
+      {
+        key = "<leader>gc";
+        action = {
+          __raw = ''
+            function()
+              local neogit = require('neogit')
+              neogit.open({ "commit" })
+            end
+          '';
+        };
+        mode = "n";
+        options = {desc = "Git Commit";};
+      }
+      {
+        key = "<leader>gf";
+        action = {
+          __raw = ''
+            function()
+              local file = vim.fn.expand("%")
+              local neogit = require('neogit')
+              neogit.action("log", "log_current", { "--", file, })()
+            end
+          '';
+        };
+        mode = "n";
+        options = {desc = "Git Log (File)";};
+      }
+    ];
   };
 }
