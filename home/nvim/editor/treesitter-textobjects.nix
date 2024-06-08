09523541b045ccafedecba4804877812a7d35a21
ColeTrammer <cole.trammer@gmail.com>
2024-06-07 20:17:32 -0700

    Remove treesitter text object ;/, overrides

--- home/nvim/editor/treesitter-textobjects.nix
+++ home/nvim/editor/treesitter-textobjects.nix
@@ -42,18 +42,5 @@
         };
       };
     };
-
-    extraConfigLua = ''
-      local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-
-      -- Repeat movement with ; and ,
-      -- ensure ; goes forward and , goes backward regardless of the last direction
-      -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-      -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-      -- vim way: ; goes to the direction you were moving.
-      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-    '';
   };
 }
