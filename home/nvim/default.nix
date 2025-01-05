b32f44b74719693f6a3bc61642e4b8050a8bd3a1
ColeTrammer <cole.trammer@gmail.com>
2025-01-05 00:18:12 -0800

    Force neovim to use latest tree sitter version
    Turns out version 0.24.4 is broken (either in general or with neovim
    10.3). This caused neovim to freeze up constantly when deleting text.
    Until this fix makes it through to nixos-unstable, we have to compile
    treesitter + nvim from source.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -16,44 +16,76 @@
     };
   };
 
-  config = lib.mkIf config.nvim.enable {
-    programs.nixvim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
+  config =
+    let
+      ts-overlay = final: prev: {
+        tree-sitter =
+          let
+            version = "0.24.6";
+            hash = "sha256-L7F2/S22knqEdB2hxfqLe5Tcgk0WQqBdFQ7BvHFl4EI=";
+            cargoHash = "sha256-mk3aw1aFu7N+b4AQL5kiaHuIAuJv24KonFeGKid427Q=";
+            src = prev.fetchFromGitHub {
+              owner = "tree-sitter";
+              repo = "tree-sitter";
+              tag = "v${version}";
+              inherit hash;
+              fetchSubmodules = true;
+            };
+          in
+          prev.tree-sitter.override {
+            rustPlatform = prev.rustPlatform // {
+              buildRustPackage =
+                args:
+                prev.rustPlatform.buildRustPackage (
+                  args
+                  // {
+                    inherit version cargoHash src;
+                  }
+                );
+            };
+          };
+      };
+    in
+    lib.mkIf config.nvim.enable {
+      nixpkgs.overlays = [ ts-overlay ];
 
-      # Trying out the experimental lua loader.
-      luaLoader.enable = true;
+      programs.nixvim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
 
-      # wl-clipboard is required for copy/paste to work on wayland desktops.
-      # ripgrep and find is used for search
-      extraPackages = with pkgs; [
-        wl-clipboard
-        ripgrep
-        fd
-      ];
+        # Trying out the experimental lua loader.
+        luaLoader.enable = true;
+
+        # wl-clipboard is required for copy/paste to work on wayland desktops.
+        # ripgrep and find is used for search
+        extraPackages = with pkgs; [
+          wl-clipboard
+          ripgrep
+          fd
+        ];
+      };
+
+      # Aliases
+      home.shellAliases = {
+        vimdiff = "nvim -d";
+        dvim = "echo | nvim";
+      };
+
+      # Persist nvim data.
+      home.persistence."/persist/home" = {
+        allowOther = true;
+        directories = [
+          {
+            directory = ".local/state/nvim";
+            method = "symlink";
+          }
+          {
+            directory = ".local/share/nvim";
+            method = "symlink";
+          }
+        ];
+      };
     };
-
-    # Aliases
-    home.shellAliases = {
-      vimdiff = "nvim -d";
-      dvim = "echo | nvim";
-    };
-
-    # Persist nvim data.
-    home.persistence."/persist/home" = {
-      allowOther = true;
-      directories = [
-        {
-          directory = ".local/state/nvim";
-          method = "symlink";
-        }
-        {
-          directory = ".local/share/nvim";
-          method = "symlink";
-        }
-      ];
-    };
-  };
 }
