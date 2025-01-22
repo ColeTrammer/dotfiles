7c97d731dbbaaddd8ea47cf4af547bfeed96aa2d
ColeTrammer <cole.trammer@gmail.com>
2025-01-21 18:53:43 -0800

    Update flake

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -16,79 +16,47 @@
     };
   };
 
-  config =
-    let
-      ts-overlay = final: prev: {
-        tree-sitter =
-          let
-            version = "0.24.6";
-            hash = "sha256-L7F2/S22knqEdB2hxfqLe5Tcgk0WQqBdFQ7BvHFl4EI=";
-            cargoHash = "sha256-mk3aw1aFu7N+b4AQL5kiaHuIAuJv24KonFeGKid427Q=";
-            src = prev.fetchFromGitHub {
-              owner = "tree-sitter";
-              repo = "tree-sitter";
-              tag = "v${version}";
-              inherit hash;
-              fetchSubmodules = true;
-            };
-          in
-          prev.tree-sitter.override {
-            rustPlatform = prev.rustPlatform // {
-              buildRustPackage =
-                args:
-                prev.rustPlatform.buildRustPackage (
-                  args
-                  // {
-                    inherit version cargoHash src;
-                  }
-                );
-            };
-          };
-      };
-    in
-    lib.mkIf config.nvim.enable {
-      nixpkgs.overlays = [ ts-overlay ];
+  config = lib.mkIf config.nvim.enable {
+    programs.nixvim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
 
-      programs.nixvim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
+      # Trying out the experimental lua loader.
+      luaLoader.enable = true;
 
-        # Trying out the experimental lua loader.
-        luaLoader.enable = true;
+      # Use nixpkgs in sync with home manager.
+      nixpkgs.useGlobalPackages = true;
 
-        # Use nixpkgs in sync with home manager.
-        nixpkgs.useGlobalPackages = true;
-
-        # wl-clipboard is required for copy/paste to work on wayland desktops.
-        # ripgrep and find is used for search
-        extraPackages = with pkgs; [
-          wl-clipboard
-          ripgrep
-          fd
-        ];
-      };
-
-      # Aliases
-      home.shellAliases = {
-        vimdiff = "nvim -d";
-        dvim = "echo | nvim";
-      };
-
-      # Persist nvim data.
-      home.persistence."/persist/home" = {
-        allowOther = true;
-        directories = [
-          {
-            directory = ".local/state/nvim";
-            method = "symlink";
-          }
-          {
-            directory = ".local/share/nvim";
-            method = "symlink";
-          }
-        ];
-      };
+      # wl-clipboard is required for copy/paste to work on wayland desktops.
+      # ripgrep and fd is used for search
+      extraPackages = with pkgs; [
+        wl-clipboard
+        ripgrep
+        fd
+      ];
     };
+
+    # Aliases
+    home.shellAliases = {
+      vimdiff = "nvim -d";
+      dvim = "echo | nvim";
+    };
+
+    # Persist nvim data.
+    home.persistence."/persist/home" = {
+      allowOther = true;
+      directories = [
+        {
+          directory = ".local/state/nvim";
+          method = "symlink";
+        }
+        {
+          directory = ".local/share/nvim";
+          method = "symlink";
+        }
+      ];
+    };
+  };
 }
