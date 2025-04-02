c66f3248c7501194ac7d86735382990a246efca6
ColeTrammer <cole.trammer@gmail.com>
2025-04-01 22:46:34 -0700

    Update flake

--- home/nvim/ui/image.nix
+++ home/nvim/ui/image.nix
@@ -2,21 +2,23 @@
   programs.nixvim = {
     plugins.image = {
       enable = true;
-      editorOnlyRenderWhenFocused = true;
-      tmuxShowOnlyInActiveWindow = true;
-      hijackFilePatterns = [
-        "*.png"
-        "*.jpg"
-        "*.jpeg"
-        "*.gif"
-        "*.webp"
-        "*.avif"
-        "*.svg"
-      ];
-      integrations = {
-        markdown = {
-          clearInInsertMode = true;
-          onlyRenderImageAtCursor = true;
+      settings = {
+        editor_only_render_when_focused = true;
+        tmux_show_only_in_active_window = true;
+        hijack_file_patterns = [
+          "*.png"
+          "*.jpg"
+          "*.jpeg"
+          "*.gif"
+          "*.webp"
+          "*.avif"
+          "*.svg"
+        ];
+        integrations = {
+          markdown = {
+            clear_in_insert_mode = true;
+            only_render_image_at_cursor = true;
+          };
         };
       };
     };
